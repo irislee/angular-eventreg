1332a8eb6f703fc8130df29f2c51e6facaacb33b
irislee <irislee.music@gmail.com>
2018-07-29 19:50:46 -0400

    add sessions to event

--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -12,7 +12,30 @@ eventsApp.controller('EventController',
         city: 'Mountain View',
         province: 'CA'
       },
-      imageUrl: '/img/angularjs-logo.png'
+      imageUrl: '/img/angularjs-logo.png',
+      sessions: [
+        {
+          name: 'Directives Masterclass',
+          creatorName: 'Bob',
+          duration: '1 hr',
+          level: 'Advanced',
+          abstract: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.'
+        },
+        {
+          name: 'Scopes for fun and profit',
+          creatorName: 'John',
+          duration: '30 mins',
+          level: 'Introductory',
+          abstract: 'Accusamus consectetur iure, sequi rem voluptas impedit dolore.'
+        },
+        {
+          name: 'Well Behaved Controllers',
+          creatorName: 'Molly',
+          duration: '2 hrs',
+          level: 'Intermediate',
+          abstract: 'Nemo adipisci incidunt iure iusto tenetur voluptatibus ducimus.'
+        }
+      ]
     }
 
   }
