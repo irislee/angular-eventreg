aa3f3a8ceb71986067a8bb2f70981e53dde3d084
irislee <irislee.music@gmail.com>
2018-07-31 16:45:33 -0400

    new event two way binding

--- app/js/controllers/EditEventController.js
+++ app/js/controllers/EditEventController.js
@@ -2,6 +2,13 @@
 
 eventsApp.controller('EditEventController',
   function EditEventController($scope) {
+    $scope.saveEvent = function(event) {
+      window.alert('event "' + event.name + '" saved!');
+    };
+
+    $scope.cancelEdit = function() {
+      window.location="/eventdetails.html";
+    };
 
   }
 );
