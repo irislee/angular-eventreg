93100cf9a570acb8e82123a6203e9c6a9db0e34f
irislee <irislee.music@gmail.com>
2018-08-01 15:25:16 -0400

    event data to $http

--- app/js/services/EventData.js
+++ app/js/services/EventData.js
@@ -1,42 +1,14 @@
-eventsApp.factory('eventData', function () {
+eventsApp.factory('eventData', function ($http, $log) {
 
   return {
-    event: {
-      name: 'Angular Boot Camp',
-      date: '1/1/2019',
-      time: '10:30 am',
-      location: {
-        address: 'Google Headquarters',
-        city: 'Mountain View',
-        province: 'CA'
-      },
-      imageUrl: '/img/angularjs-logo.png',
-      sessions: [
-        {
-          name: 'Directives Masterclass',
-          creatorName: 'Bob',
-          duration: 1,
-          level: 'Advanced',
-          abstract: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
-          upVoteCount: 0
-        },
-        {
-          name: 'Scopes for fun and profit',
-          creatorName: 'John',
-          duration: 2,
-          level: 'Introductory',
-          abstract: 'Accusamus consectetur iure, sequi rem voluptas impedit dolore.',
-          upVoteCount: 0
-        },
-        {
-          name: 'Well Behaved Controllers',
-          creatorName: 'Molly',
-          duration: 4,
-          level: 'Intermediate',
-          abstract: 'Nemo adipisci incidunt iure iusto tenetur voluptatibus ducimus.',
-          upVoteCount: 0
-        }
-      ]
+    getEvent: function(successcb) {
+      $http({method: 'GET', url: '/data/event/1'}).
+        success(function(data, status, headers, config) {
+          successcb(data);
+        }).
+        error(function(data, status, headers, config) {
+          $log.warn(data, status, headers, config);
+        });
     }
   };
 
