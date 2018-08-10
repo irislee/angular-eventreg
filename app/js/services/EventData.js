f6edc3dc07683bbeb063f1f91cc24c6fc0d4394f
irislee <irislee.music@gmail.com>
2018-08-10 12:23:22 -0400

    inject $resourcee

--- app/js/services/EventData.js
+++ app/js/services/EventData.js
@@ -1,8 +1,8 @@
-eventsApp.factory('eventData', function ($http) {
+eventsApp.factory('eventData', function ($resource) {
 
   return {
     getEvent: function() {
-      return $http({method: 'GET', url: '/data/event/1'});
+      return $resource('/data/event/:id', {id:'@id'}).get({id:1});
     }
   };
 
